7b2e6fbc7889e43833677dce45b9ba74d282f304
VladyslavKozlov2101git <60036579+VladyslavKozlov2101git@users.noreply.github.com>
2022-05-12 10:35:22 +0300

    first commit

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,19 @@
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() CreateTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(CreateTaskDto);
+  }
+}
