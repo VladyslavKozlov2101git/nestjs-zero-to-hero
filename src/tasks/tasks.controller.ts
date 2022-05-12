090c2784bf88c61c7b15e6d315b3deff868a619f
VladyslavKozlov2101git <60036579+VladyslavKozlov2101git@users.noreply.github.com>
2022-05-12 21:38:21 +0300

    22. Feature Searching and Filtering

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,3 @@
-import { CreateTaskDto } from './dto/create-task.dto';
 import {
   Body,
   Controller,
@@ -7,17 +6,24 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
-import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task, TaskStatus } from './task.model';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
   }
 
   @Get('/:id')
@@ -25,21 +31,21 @@ export class TasksController {
     return this.tasksService.getTaskById(id);
   }
 
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
   @Delete('/:id')
   deleteTask(@Param('id') id: string): void {
     return this.tasksService.deleteTask(id);
   }
 
-  @Post()
-  createTask(@Body() CreateTaskDto: CreateTaskDto): Task {
-    return this.tasksService.createTask(CreateTaskDto);
-  }
-
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Param('status') status: TaskStatus,
-  ) {
+    @Body('status') status: TaskStatus,
+  ): Task {
     return this.tasksService.updateTaskStatus(id, status);
   }
 }
